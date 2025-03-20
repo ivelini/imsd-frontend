915a2a5989ff9227df83390d3c5bdd201d69d61e
Ilya Perminov <perminov.ilya@4px.ru>
2025-03-20 12:11:02 +0500

    Редирект с вендора на фильтр

--- src/app/catalog/[entity]/[vendor]/page.jsx
+++ src/app/catalog/[entity]/[vendor]/page.jsx
@@ -1,19 +1,48 @@
 "use client"
 
 import { useRouter, useParams, useSearchParams } from "next/navigation";
+import {useStore} from "@/store/useStore";
+import {useEffect, useState} from "react";
+import BackendApi from "@/lib/BackendApi";
+import {TypeProductEnum} from "@/lib/TypeProductEnum";
 
-/**
- * @param {Object} params 
- * @param {string} params.entity
- * @param {string} params.vendor
- */
+
 export default function Vendor() {
     const router = useRouter() 
     const searchParams = useSearchParams();
+
+    /**
+     * @type {{ entity?: string, vendor?: string }}
+     */
     const params = useParams();
+    const {setParamFilterTires, setParamFilterWheels, clearFilter} = useStore()
 
+    useEffect(() => {
+        clearFilter({entity: 'filterTires', param: 'params'})
+        clearFilter({entity: 'filterWheels', param: 'params'})
 
-    console.log(params)
+        if(searchParams.get('vendor') !== null) {
+
+            (async () => {
+                const response = await BackendApi.get('/api/list/filter/vendor', {
+                    type: params.entity,
+                    slug: params.vendor
+                })
+
+                if (response.code === 200) {
+                    let data = (await response).data
+
+                    if(params.entity === TypeProductEnum.TIRES) setParamFilterTires({type:'vendor', value: data[0].id})
+                    if(params.entity === TypeProductEnum.DISKS) setParamFilterWheels({type:'vendor', value: data[0].id})
+
+                    router.push(`/${params.entity}_selection`)
+                }
+            })()
+        } else {
+            router.push(`/${params.entity}_selection`)
+        }
+
+    }, [])
 
 
 }
