6a8641cd4c79d084b78da7f5a98a7635fb5325bc
Ilya Perminov <ivelini@yandex.ru>
2025-09-17 12:54:44 +0500

    Revert "Счетчик метрики"
    This reverts commit 732184e7cd12ae7f8496037db4f3e99d598bb485.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,6 @@ import 'primeicons/primeicons.css';
 
 import HeaderSection from "@/app/_components/layout/HeaderSection";
 import FooterSection from "@/app/_components/layout/FooterSection";
-import Script from "next/script";
 
 
 export default function RootLayout({ children }) {
@@ -26,22 +25,6 @@ export default function RootLayout({ children }) {
                         <FooterSection />
                     </div>
                 </div>
-
-
-                <Script
-                    id="yandex-metrika"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(m,e,t,r,i,k,a){
-                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();
-                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104181214', 'ym');
-
-                    ym(104181214, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`
-                    }}/>
-                <noscript><div><img src="https://mc.yandex.ru/watch/104181214" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
             </body>
         </html>
     )
