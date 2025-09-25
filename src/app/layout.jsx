17500ae627d40215bc7dae96b19a7386ec003f81
Ilya Perminov <ivelini@yandex.ru>
2025-09-25 17:10:11 +0500

    Счетчик google

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -54,7 +54,6 @@ export default function RootLayout({children}) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-            
               gtag('config', 'G-EKJLM8DLJ9');`
                 }}/>
         </head>
