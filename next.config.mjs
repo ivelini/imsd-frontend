73f4f0a87fccd01f1a87fd9f959b728311e95fab
Ilya Perminov <ivelini@yandex.ru>
2025-09-30 09:18:43 +0500

    Добавление строки запроса

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,8 @@ const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        domains: ['api.aalyans74.ru', 'aalyans74.ru'],
-    },
-    styledComponents: true
+        domains: ['api.aalyans74.ru', 'aalyans74.ru', 'api.aalyans.ru', 'aalyans.ru'],
+    }
 };
 
 export default nextConfig;
