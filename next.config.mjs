0a5dfb618e745a0d03dde5eb70d1380e1bb4649d
Ilya Perminov <ivelini@yandex.ru>
2026-04-21 16:18:10 +0500

    деплой

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        domains: ['api.aalyans74.ru', 'aalyans74.ru', 'api.aalyans.ru', 'aalyans.ru'],
+        domains: ['api.aalyans.ivelini.ru', 'aalyans.ivelini.ru', 'api.aalyans.ru', 'aalyans.ru'],
     }
 };
 
